3ca9258d2cb1de904450ddec980bd77c5e5bd1f9
tejasMetricoidtech <tejas@metricoidtech.com>
2022-02-08 20:37:45 +0530

    commit

---
+++ src/components/ManeTable.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManeTable = () => {
+    return (
+        <div>
+            <h1>Mane table</h1>
+        </div>
+    )
+};
+
+export default ManeTable;
---
+++ src/utils/apiEndpoint.js
@@ -0,0 +1,1 @@
+export const BASE_URL = 'https://c0ri699qs5.execute-api.us-east-1.amazonaws.com/v1';
